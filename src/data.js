31759594a84131b7f13f386c0264a36695f06ca6
Mathew Ogden <mathewjohn.ogden@gmail.com>
2021-12-10 10:14:07 -0800

    First commit

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const projects = [
+  {
+    title: "Ecommerce Frontend",
+    subtitle: "React, Bootstrap, Reactstrap",
+    description:
+      "A functioning ecommerce platform where users can buy or sell items, manage their listings, & checkout (using local storage). Uses Axios for making API calls. Checkout the code!",
+    image: "./frontend.gif",
+    link: "https://github.com/mathewogden/Ecommerce-Example-Frontend",
+  },
+  {
+    title: "Ecommerce Backend",
+    subtitle: "Express, MySQL/Sequelize",
+    description:
+      "Uses JWT Token authentication. Has all the necessary CRUD operations for users, products, and orders. Upon checkout, the DB's inventory is successfully updated. Checkout the code!",
+    image: "./backend.gif",
+    link: "https://github.com/mathewogden/Ecommerce-Backend-Example",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "Node.js / Express",
+  "React",
+  "Angular",
+  "MySQL / MongoDB",
+  "CSS / HTML",
+];
